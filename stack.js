58e63d4476324b103e18c0033d84ed20cfacee27
CharleeWa <18888351756@163.com>
2023-05-06 18:01:43 +0800

    chore: 对栈的一些用例

--- stack.js
+++ stack.js
@@ -41,22 +41,72 @@ function clear() {
   this.top = 0
 }
 
-var s = new Stack()
+// var s = new Stack()
 
-s.push('David')
-s.push('Raymond')
-s.push('Bryan')
-console.log('length: ' + s.length())
-console.log(s.peek())
-console.log(s.top)
+// s.push('David')
+// s.push('Raymond')
+// s.push('Bryan')
+// console.log('length: ' + s.length())
+// console.log(s.peek())
+// console.log(s.top)
 
-var popped = s.pop()
-console.log('The popped element is:' + popped)
-console.log(s.peek())
-s.push('Cynthia')
-console.log(s.peek())
-s.clear()
-console.log('length: ' + s.length())
-console.log(s.peek())
-s.push('Clayton')
-console.log(s.peek())
+// var popped = s.pop()
+// console.log('The popped element is:' + popped)
+// console.log(s.peek())
+// s.push('Cynthia')
+// console.log(s.peek())
+// s.clear()
+// console.log('length: ' + s.length())
+// console.log(s.peek())
+// s.push('Clayton')
+// console.log(s.peek())
+
+// 利用栈将数字转换为二至九进制的数字
+// function mulBase(num, base) {
+//   var s = new Stack()
+//   do {
+//     s.push(num % base)
+//     num = Math.floor(num /= base)
+//   } while (num > 0)
+//   var converted = ''
+//   while (s.length() > 0) {
+//     converted += s.pop()
+//   }
+//   return converted
+// }
+
+// 利用栈，判断一个字符串是否是回文
+
+function isPalindrome(word) {
+  var s = new Stack()
+  for (var i = 0; i < word.length; i++) {
+    s.push(word[i])
+  }
+  var rword = ''
+  while (s.length() > 0) {
+    rword += s.pop()
+  }
+  if (word === rword) {
+    return true
+  } else {
+    return false
+  }
+}
+
+var word = 'racecar'
+if (isPalindrome(word)) {
+  console.log(word + ' is a palindrome.')
+} else {
+  console.log(word + ' is not a palindrome.')
+}
+
+// 栈可以用来判断一个算术表达式中的括号是否匹配。编写一个函数，该函数接受一个算术表达式作为参数，
+// 返回括号缺失的位置。下面是一个括号不匹配的算术表达式的例子：2.3 + 23 / 12 + (3.14159×0.24。
+
+// 一个算术表达式的后缀表达式形式如下：op1 op2 operator，
+// 使用两个栈，一个用来存储操作数，另一个用来存储操作符，设计并实现一个JavaScript函数，
+// 该函数可以将中缀表达式转换为后缀表达式，然后利用栈对该表达式求值。
+
+// 现实生活中栈的一个例子是佩兹糖果盒。想象一下你有一盒佩兹糖果，里面塞满了红色、黄色和白色的糖果，
+// 但是你不喜欢黄色的糖果。使用栈（有可能用到多个栈）写一段程序，在不改变盒内其他糖果叠放顺序的基础上，
+// 将黄色糖果移出。
