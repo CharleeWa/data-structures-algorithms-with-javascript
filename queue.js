6a5814b3e4d36ef6c148d1d255e73c3a52ecfa57
CharleeWa <18888351756@163.com>
2023-05-21 16:17:47 +0800

    chore: 优先队列

--- queue.js
+++ queue.js
@@ -107,3 +107,55 @@ if (!femaleDancers.empty()) {
 if (!maleDancers.empty()) {
   print(maleDancers.front().name + " is waiting to dance.");
 }
+
+
+// 优先队列
+
+function Patient(name, code) {
+  this.name = name;
+  this.code = code;
+}
+
+function dequeue() {
+  var entry = 0;
+  for (var i = 0; i < this.dataStore.length; i++) {
+    if (this.dataStore[i].code < this.dataStore[entry].code) {
+      entry = i;
+    }
+  }
+  return this.dataStore.splice(entry, 1);
+}
+
+function toString() {
+  var retStr = '';
+  for (var i = 0; i < this.dataStore.length; i++) {
+    retStr += this.dataStore[i].name + ' code: ' + this.dataStore[i].code + '\n';
+  }
+  return retStr;
+}
+
+var p = new Patient('Smith', 5);
+var ed = new Queue();
+ed.enqueue(p);
+p = new Patient('Jones', 4);
+ed.enqueue(p);
+p = new Patient('Fehrenbach', 6);
+ed.enqueue(p);
+p = new Patient('Brown', 1);
+ed.enqueue(p);
+p = new Patient('Ingram', 1);
+ed.enqueue(p);
+console.log(ed.toString());
+var seen = ed.dequeue();
+console.log('Patient being treated: ' + seen[0].name);
+console.log('Patients waiting to be seen: ');
+console.log(ed.toString());
+// 下一轮
+var seen = ed.dequeue();
+console.log('Patient being treated: ' + seen[0].name);
+console.log('Patients waiting to be seen: ');
+console.log(ed.toString());
+var seen = ed.dequeue();
+console.log('Patient being treated: ' + seen[0].name);
+console.log('Patients waiting to be seen: ');
+console.log(ed.toString());
